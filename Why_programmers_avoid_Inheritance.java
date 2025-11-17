2e0b872bd9912c1320c6e7f1a576f3125dba379b
maruf <maruf.cuet.cse16@gmail.com>
2025-11-18 00:44:37 +0600

    Why programmers avoid Inheritance

---
+++ Why_programmers_avoid_Inheritance.java
@@ -0,0 +1,73 @@
+public class Image {
+    pixels[][]
+    public Pixel getPixel(int x, int y){
+        return pixels[x][y];
+    }
+    public void verticalFlip(){
+    }
+    public void horizontalFlip(){
+    }
+    public void resize(){
+        // resize image by scale
+    }
+    abstract save();
+    abstract load();
+}
+
+public class PNG extends Image{
+    save(){ \\ override }
+    load(){ \\ override }
+}
+
+public class JPG extends Image{
+    save(){ \\ override }
+    load(){ \\ override }
+}
+public class BTTMAP extends Image{
+    save(){ \\ override }
+    load(){ \\ override }
+}
+
+public class Drawablw extends Image{
+    save(){ throw exeption }
+    load(){ throw exeption }
+}
+
+
+// From Client code
+Image image = new Drawablw();
+image.load();
+// this will give, exception. Currently client is not ready for handling exception. It will create big problem.
+
+
+// if we change a little bit like below
+public class FileImage extends Image{
+    save(){ \\ override }
+    load(){ \\ override }
+}
+ // then we need to change all of the class that extends Image to extends FileImage
+ public class JPG extends FileImage{
+     save(){ \\ override }
+     load(){ \\ override }
+ }
+public class BTTMAP extends FileImage{
+    save(){ \\ override }
+    load(){ \\ override }
+}
+
+public class Drawablw {
+
+}
+
+// and client also needs to change FileImage every where it uses Image
+// From Client code
+FileImage image = new Drawablw();
+image.load();
+
+
+// So new requirement/changes makes it hard to maintain. It does not gives flexibility.
+1. Exception occure / new exception handling method needed
+2. new requirement require lots of changes to make it works
+
+that's why we should avoid inheritance.
+We should use composition + interface over inheritance
